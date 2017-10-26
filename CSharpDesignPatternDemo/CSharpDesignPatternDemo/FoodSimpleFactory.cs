13a5d9e33b38aaa57eed5209551e1b5b84bdd61b
cmf <1176023110@qq.com>
2017-10-26 20:13:13 +0800

    添加简单工厂模式实现

---
+++ CSharpDesignPatternDemo/CSharpDesignPatternDemo/FoodSimpleFactory.cs
@@ -0,0 +1,77 @@
+﻿using CSharpDesignPatternDemo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CSharpDesignPatternDemo
+{
+    public class FoodSimpleFactory
+    {
+        FoodSimpleFactory()
+        {
+            //客户想吃西红柿炒鸡蛋
+            Food food1 = FoodSimpleFactoryClass.CreateFood("西红柿炒鸡蛋");
+            food1.Print();
+
+            //客户想再点一个土豆肉丝
+            Food food2 = FoodSimpleFactoryClass.CreateFood("土豆肉丝");
+            food2.Print();
+
+            Console.Read();
+        }
+    }
+
+    /// <summary>
+    /// 菜抽象类
+    /// </summary>
+    public abstract class Food
+    {
+        //输出点了什么菜
+        public abstract void Print();
+    }
+
+    /// <summary>
+    /// 西红柿炒鸡蛋这道菜
+    /// </summary>
+    public class TomatoScrambleEggs : Food
+    {
+        public override void Print()
+        {
+            Console.WriteLine("一份西红柿炒鸡蛋！");
+        }
+    }
+
+    /// <summary>
+    /// 土豆肉丝这道菜
+    /// </summary>
+    public class ShreddedPorkWithPotatoes:Food
+    {
+        public override void Print()
+        {
+            Console.WriteLine("一份土豆肉丝！");
+        }
+    }
+
+    /// <summary>
+    /// 简单工厂类，负责 炒菜
+    /// </summary>
+    public class FoodSimpleFactoryClass
+    {
+        public static Food CreateFood(string type)
+        {
+            Food food = null;
+            if(type=="土豆肉丝")
+            {
+                food = new ShreddedPorkWithPotatoes();
+            }
+            else if(type=="西红柿炒鸡蛋")
+            {
+                food = new TomatoScrambleEggs();
+            }
+
+            return food;
+        }
+    }
+
+}
---
+++ CSharpDesignPatternDemo/CSharpDesignPatternDemo/Singleton.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CSharpDesignPatternDemo
+{
+    /// <summary>
+    /// Singleton.xaml 的交互逻辑
+    /// </summary>
+    public partial class Singleton : Window
+    {
+        public Singleton()
+        {
+            InitializeComponent();
+        }
+
+        #region 单线程处理，单例模式
+        /// <summary>
+        /// 定义一个静态变量来保存类的实例
+        /// </summary>
+        private static Singleton uniqueInstance;
+        /// <summary>
+        /// 定义公有方法提供一个全局的访问点，同时你也可以定义公有属性变量来提供全局访问点
+        /// </summary>
+        /// <returns></returns>
+        public static Singleton GetInstance()
+        {
+            if (uniqueInstance == null)
+            {
+                uniqueInstance = new Singleton();
+            }
+            return uniqueInstance;
+        }
+        #endregion
+
+
+        #region 多线程处理，单例模式，推荐使用
+        /// <summary>
+        /// 定义一个静态变量来保存类的实例
+        /// </summary>
+        private static Singleton uniqueInstance2;
+        /// <summary>
+        /// 定义一个标识确保线程同步
+        /// </summary>
+        private static readonly object locker = new object();
+        /// <summary>
+        /// 定义公有方法提供一个全局的访问点，同时你也可以定义公有属性变量来提供全局访问点
+        /// </summary>
+        /// <returns></returns>
+        public static Singleton GetInstance2()
+        {
+            // 当第一个线程运行到这里时，此时会对locker对象 "加锁"，
+            // 当第二个线程运行该方法时，首先检测到locker对象为"加锁"状态，该线程就会挂起等待第一个线程解锁
+            // lock语句运行完之后（即线程运行完之后）会对该对象"解锁"
+            // 双重锁定只需要一句判断就可以了
+            if (uniqueInstance2==null)
+            {
+                lock(locker)
+                {
+                    if (uniqueInstance2 == null)
+                    {
+                        uniqueInstance2 = new Singleton();
+                    }
+                }
+            }
+            return uniqueInstance2;
+        }
+        #endregion
+    }
+}
